e6f9e76a6ab68fb91ccaf37d7c615b4d735b8615
Valter Jose <valterjosecc@gmail.com>
2026-05-09 12:27:48 -0300

    Adiciona estudos de métodos string

---
+++ notas/metódos_de_strings.py
@@ -0,0 +1,30 @@
+# .upper() -→ Transformar as letras da frase toda em maiúsculo 
+print('Olá'.upper())
+
+# .lower() -→ Transformar as letras da frase toda em minúsculo 
+print('OLá'.lower())
+
+# .count() -→ verifica a quantidades de letras dessa frase
+print('Olá, meu nome é valter, qual é o seu nome'.count(''))
+# Tambem pode verificar um caracter expecifico ou uma frase
+
+# .startswith() -→ Verifica se uma string começa com determinado texto.
+sair = input('Deseja sair do programa? [s]im [n]ão').startswith('s')
+print(sair)
+# True  → se começar 's' com isso
+# False → se não
+
+
+
+
+# ┌ ─ ┐
+# │   │
+# └ ─ ┘
+
+# ┌→ 
+# ├→
+# └→ 
+
+# ┌──┐
+# ├──┤
+# └──┘ 
